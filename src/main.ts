7dab629ee9d7fde213c30577994796ef21063c2c
Rodrigo Munoz Campos <rpemcampos@gmail.com>
2018-12-02 22:04:37 -0300

    cambios

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 declare const module: any;
 async function bootstrap() {
+  let port =process.env.PORT;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.listen(3000).then((value)=>{
+  app.listen(port || 3001).then((value)=>{
     if (module.hot) {
       module.hot.accept();
       module.hot.dispose(() => app.close());
